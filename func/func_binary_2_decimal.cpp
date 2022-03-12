47c7670dae290b689008ec65cd45382251e079b6
harsh-haria <harshharia1234@gmail.com>
2022-03-13 01:16:09 +0530

    12-03

--- func/func_binary_2_decimal.cpp
+++ func/func_binary_2_decimal.cpp
@@ -1,20 +1,32 @@
 #include<iostream>
 #include<string>
+#include<cmath>
 using namespace std;
 
-int deci=0;
-int bi;
-string x;
-char code[32] = {0};
-
-void spliter(string s){
-    cout<<s[3]<<endl;
+int main()
+{
+    string s;
+    cin>>s;
+    int sum=0;
+    int power=0;
+    int len = s.length();
+    // cout<<len<<endl;
+    while(len>0){
+        char p = s[len-1];
+        // (int)p==48?cout<<48<<" ":cout<<49<<" ";
+        if((int)p==48){
+            sum += 0;
+        }
+        if((int)p==49){
+            sum += pow(2,power);
+        }
+        power++;
+        len--;
+    }
+        
+    cout<<sum;
+    // char p = s[0];
+    // cout<<(int)p<<endl;
     
-}
 
-int main(){
-    cout<<"Enter the binary number: ";
-    getline(cin,x);
-    cout<<"Number Entered: "<<x<<endl;
-    spliter(x);
 }
---
+++ func/func_octal_2_decimal.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int oct2dec(string s,int len){
+    int sum = 0;
+
+    
+
+    return sum;
+
+}
+
+int main(){
+    string str;
+    cin>>str;
+    cout<< oct2dec(str,str.length())<<endl;
+}
