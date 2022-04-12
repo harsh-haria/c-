b6a1d06524aaa6578f21bc890603cf1a2e7d0653
harsh-haria <harshharia1234@gmail.com>
2022-04-13 01:01:20 +0530

    12-04

---
+++ basic/linked_list_intro.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+using namespace std;
+
+class node{
+    public:
+        int data;
+        node * next;
+
+        node(int n){
+            data = n;
+            next = NULL;
+        }
+};
+void insertAtHead(node * &head, int val){
+    node * n = new node(val);
+    n->next = head;
+    head = n;
+}
+
+void insertAtTale(node * &head, int val){// &head bcz we will be modifying the head
+
+    node * n = new node(val);
+
+    if(head==NULL){
+        head = n;
+        return;
+    }
+
+    node * temp = head;
+    while(temp->next!=NULL){
+        temp = temp->next;
+    }
+    temp->next = n;
+    //no need to put null in n->next bcz we have already did it in constructor
+}
+
+void display(node * head){// only head here bcz we will just traverse the list
+    node * temp = head; //no need to do this. we could have also used head as we are just taking the refernce of head
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp = temp->next;
+    }
+    cout<<"NULL"<<endl;
+}
+
+bool searchList(node * head, int key){
+    node * temp = head;
+    while(temp!=NULL){
+        if(temp->data==key){
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+void deleteNode(node * &head, int val){
+
+    if(head==NULL){
+        cout<<"Nothing to Delete"<<endl;
+        return;
+    }
+
+    node * temp = head;
+    if(temp->data==val){
+        node * headtodelete = head;
+        head = temp->next;
+        delete headtodelete; //helps to avoid memory leak
+        return;
+    }
+    while((temp->next)->data!=val){
+        temp = temp->next;
+    }
+    node * todelete = temp->next;
+    temp->next = (temp->next)->next;
+    delete todelete; //works like free() we had in c which is used to free the space in heap
+}
+
+void deleteNode(node* temp){
+    if(temp->next==NULL){
+        delete temp;
+    }
+    while(temp->next->next!=NULL){
+
+    }
+}
+
+// int main(){
+//     node * head = NULL;
+//     insertAtTale(head,1);
+//     insertAtTale(head,2);
+//     insertAtTale(head,3);
+//     display(head);
+//     insertAtHead(head,0);
+//     display(head);
+//     insertAtHead(head,4);
+//     display(head);
+//     cout<<searchList(head, 5)<<endl;
+//     cout<<searchList(head,0)<<endl;
+//     deleteNode(head,3);
+//     display(head);
+//     deleteNode(head,1);
+//     display(head);
+//     deleteNode(head,4);
+//     display(head);
+// }
--- basic/test.cpp
+++ basic/test.cpp
@@ -1,36 +1,51 @@
 #include<iostream>
 #include<string>
 #include<algorithm>
+#include "linked_list_intro.cpp"
+
 using namespace std;
 
 class ListNode{
     public:
     int val;
     ListNode *next;
+
     ListNode(int x=0){
         val = x;
         next = NULL;
     }
-
 };
 
-void insertAtTale(ListNode * &head, int val){// &head bcz we will be modifying the head
-
-    ListNode * n = new ListNode(val);
-
+void InsertNode(ListNode* &head, int val){
+    ListNode* n = new ListNode(val);
     if(head==NULL){
         head = n;
         return;
     }
-
-    ListNode * temp = head;
+    ListNode* temp = head;
     while(temp->next!=NULL){
         temp = temp->next;
     }
     temp->next = n;
-    //no need to put null in n->next bcz we have already did it in constructor
+    return;
+}
+
+ListNode* sortList(ListNode* head) {
+    ListNode* ans;
+    ListNode* temp = head;
+    vector<int> arr;
+    while(temp!=NULL){
+        arr.push_back(temp->val);
+        temp = temp->next;
+    }
+    sort(arr.begin(),arr.end());
+    for(auto i:arr){
+        InsertNode(ans,i);
+    }
+    cout<<endl;
+    return ans;
 }
 
 int main(){
-    ListNode * head = NULL;
+    ListNode* head = NULL;
 }
