bf7369621fad06159ca218b9d92c98eadced997e
harsh-haria <harshharia1234@gmail.com>
2022-04-26 23:44:04 +0530

    26-04

--- DS/bt_intro.cpp
+++ DS/bt_intro.cpp
@@ -13,7 +13,48 @@ class node{
     }
 };
 
+node* createNode(int x){
+    node* temp = new node(x);
+    return temp;
+}
+
+void traverse_inorder(node* root){
+    if(root){
+        traverse_inorder(root->left);
+        cout<<root->data<<" ";
+        traverse_inorder(root->right);
+    }
+}
+
+void traverse_preorder(node* root){
+    if(root){
+        cout<<root->data<<" ";
+        traverse_preorder(root->left);
+        traverse_preorder(root->right);
+    }
+}
+
+void traverse_postorder(node* root){
+    if(root){
+        traverse_postorder(root->left);
+        traverse_postorder(root->right);
+        cout<<root->data<<" ";
+    }
+}
 
 int main(){
+    node* root = new node(1);
+    // cout<<root->data<<endl;
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    root->right->left = createNode(6);
+    root->right->right = createNode(7);
+    traverse_preorder(root);
+    cout<<endl;
+    traverse_inorder(root);
+    cout<<endl;
+    traverse_postorder(root);
 
 }
